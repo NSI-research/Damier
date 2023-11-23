bdfc2424588575349ac79c9012b47b5082b8b547
Anto-22 <144241420+Anto-22@users.noreply.github.com>
2023-11-23 18:22:12 +0100

    Add files via upload

--- Damier.py
+++ Damier.py
@@ -1,32 +1,30 @@
-from verificator import *
 from turtle import *
-import time
-
-reponse = intVerif("Donnez un nombre pair : ")
-if reponse is not False:
-    if reponse < 0:
-        print("Vous avez renseigner un nombre négatif, merci de réhitérer.")
-    elif reponse % 2 == 0:
-        color("black")
-        speed(1)
-        casepaire = 0
-        for i in range(reponse):
-            for j in range(reponse):
-                if casepaire % 2 == 0:
-                    begin_fill()
-                for k in range(4):
-                    forward(40)
-                    right(90)
-                forward(40)
-                casepaire += 1
-                end_fill()
+def damier(nbcases):
+    if nbcases%2 != 0:
+        print('entrez un nombre de lignes paire')
+        return
+    Lc = 40
+    up()
+    color('black')
+    right(135)
+    forward((nbcases*Lc*2**(1/2))/2)
+    left(135)
+    down()
+    for i in range(nbcases):
+        for j in range(nbcases//2):
+            for k in range(5):
+                forward(Lc)
+                left(90)
+            right(90)
+            begin_fill()
+            for k in range(5):
+                forward(Lc)
+                left(90)
+            end_fill()
+            right(90)
+        if i%2 == 0:
+            left(90)
+            forward(2*Lc)
+            left(90)
+        else:
             right(180)
-            forward(40 * reponse)
-            left(90)
-            forward(40)
-            left(90)
-            casepaire += 1
-    else:
-        print("pas pair")
-ht()
-time.sleep(30)
