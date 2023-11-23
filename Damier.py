2d3a0a1f246e384e8b35bfa1c5b6a091f9287623
Lodjo28 <109769264+Lodjo28@users.noreply.github.com>
2023-11-23 21:36:09 +0100

    Update Damier.py
    Update rajoutant un while pour garder l'utilisateur malgré l'erreur

--- Damier.py
+++ Damier.py
@@ -2,31 +2,35 @@ from verificator import intVerif
 from turtle import *
 import time
 
-reponse = intVerif("Donnez un nombre pair : ")
-if reponse is not False:
+Valid = False
+while Valid is False:
+    reponse = intVerif("\nDonnez un nombre pair : ")
     if reponse < 0:
-        print("Vous avez renseigner un nombre négatif, merci de réhitérer.")
-    elif reponse % 2 == 0:
-        color("black")
-        speed(1)
-        casepaire = 0
-        for i in range(reponse):
-            for j in range(reponse):
-                if casepaire % 2 == 0:
-                    begin_fill()
-                for k in range(4):
-                    forward(40)
-                    right(90)
-                forward(40)
-                casepaire += 1
-                end_fill()
-            right(180)
-            forward(40 * reponse)
-            left(90)
-            forward(40)
-            left(90)
-            casepaire += 1
+        print("\nVous avez renseigner un nombre négatif, merci de réhitérer.")
+    elif reponse % 2 != 0:
+        print("\nCe nombre n'est pas pair.")
+    elif reponse is False:
+        print("\nLe nombre que vous avez renseigné est invalide. (décimal, texte, etc...)")
     else:
-        print("Le nombre n'est pas pair !")
+        Valid = True
+
+color("black")
+speed(1000)
+casepaire = 0
+for i in range(reponse):
+    for j in range(reponse):
+        if casepaire % 2 == 0:
+            begin_fill()
+        for k in range(4):
+            forward(40)
+            right(90)
+        forward(40)
+        casepaire += 1
+        end_fill()
+    right(180)
+    forward(40 * reponse)
+    left(90)
+    forward(40)
+    left(90)
+    casepaire += 1
 ht()
-time.sleep(30)
